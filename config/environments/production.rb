131bc0bdf00c503ac56f501fea89321435be6f86
therise3107 <lyadav3107@gmail.com>
2015-03-10 21:05:36 +0530

    email for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ENV["NF_Host"] }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   config.active_job.queue_adapter = :sucker_punch
-    
+
 end
